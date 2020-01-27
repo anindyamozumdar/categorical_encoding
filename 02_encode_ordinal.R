1c48e4e93c7a84f7ed1f40e61b96d2fbd247b074
Anindya Mozumdar <radmuzom@outlook.com>
2020-01-27 23:00:11 +0800

    Ordinal encoding.

--- 02_encode_ordinal.R
+++ 02_encode_ordinal.R
@@ -1,7 +1,5 @@
 source("01_Synthetic_Data_Preparation.R")
 
-new_df <- df
-
 # Base R
 encode_ordinal <- function(x, order = unique(x)) {
   x <- as.numeric(factor(x, levels = order, exclude = NULL))
@@ -19,5 +17,6 @@ table(df[["hml"]],
       encode_ordinal(df[["hml"]], order = c("Low", "Medium", NA, "High")),
       useNA = "ifany")
 
+new_df <- df
 new_df[["hml_encoded"]] <- encode_ordinal(df[["hml"]])
 new_df[["city_encoded"]] <- encode_ordinal(df[["city"]])
