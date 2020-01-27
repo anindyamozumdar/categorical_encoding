56ffd5446f94f72b7239982a55ee4831b46648cd
Anindya Mozumdar <radmuzom@outlook.com>
2020-01-27 22:19:32 +0800

    Ordinal encoding.

--- 02_encode_ordinal.R
+++ 02_encode_ordinal.R
@@ -3,23 +3,21 @@ source("01_Synthetic_Data_Preparation.R")
 new_df <- df
 
 # Base R
-encode_ordinal <- function(x, order = unique(x), missing_lowest = FALSE) {
-  x <- as.numeric(factor(x, levels = order))
-  if (!missing_lowest) {
-    x[is.na(x)] <- max(x, na.rm = TRUE) + 1
-  } else {
-    x[is.na(x)] <- 0
-    x <- x + 1
-  }
+encode_ordinal <- function(x, order = unique(x)) {
+  x <- as.numeric(factor(x, levels = order, exclude = NULL))
   x
 }
 
 table(df[["hml"]], encode_ordinal(df[["hml"]]), useNA = "ifany")
 table(df[["hml"]],
-      encode_ordinal(df[["hml"]], missing_lowest = TRUE),
+      encode_ordinal(df[["hml"]], order = c(NA, "Low", "Medium", "High")),
       useNA = "ifany")
 table(df[["hml"]],
-      encode_ordinal(df[["hml"]], order = c("Low", "Medium", "High")),
+      encode_ordinal(df[["hml"]], order = c("Low", "Medium", "High", NA)),
+      useNA = "ifany")
+table(df[["hml"]],
+      encode_ordinal(df[["hml"]], order = c("Low", "Medium", NA, "High")),
       useNA = "ifany")
 
 new_df[["hml_encoded"]] <- encode_ordinal(df[["hml"]])
+new_df[["city_encoded"]] <- encode_ordinal(df[["city"]])
