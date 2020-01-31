e79d94b2671ac23c402bc714ebce5d9cb55cb885
Anindya Mozumdar <radmuzom@outlook.com>
2020-01-31 22:46:08 +0800

    Target encoding.

---
+++ 09_encode_target.R
@@ -0,0 +1,17 @@
+source("01_Synthetic_Data_Preparation.R")
+
+encode_target <- function(x, y) {
+  d <- aggregate(y, list(factor(x, exclude = NULL)), mean, na.rm = TRUE)
+  m <- d[is.na(as.character(d[, 1])), 2]
+  l <- d[, 2]
+  names(l) <- d[, 1]
+  l <- l[x]
+  l[is.na(l)] <- m
+  l
+}
+
+table(encode_target(df[["hml"]], df[["target"]]), df[["hml"]], useNA = "ifany")
+
+new_df <- df
+new_df[["hml_encoded"]] <- encode_target(df[["hml"]], df[["target"]])
+new_df[["city_encoded"]] <- encode_target(df[["city"]], df[["target"]])
