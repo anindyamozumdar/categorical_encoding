3efd9c04a67d39c75d8ab48baa7f3355c09e1fb0
Anindya Mozumdar <radmuzom@outlook.com>
2020-02-01 14:57:32 +0800

    Leave one out encoding.

--- 10_encode_leave_one_out.R
+++ 10_encode_leave_one_out.R
@@ -1,6 +1,6 @@
 source("01_Synthetic_Data_Preparation.R")
 
-encode_leave_one_out <- function(x, y, sigma = NULL, seed = 0) {
+encode_leave_one_out <- function(x, y, sigma = NULL) {
   n <- length(x)
   x[is.na(x)] <- "__MISSING"
   x2 <- vapply(1:n, function(i) {
@@ -11,7 +11,6 @@ encode_leave_one_out <- function(x, y, sigma = NULL, seed = 0) {
     mean(yloo, na.rm = TRUE)
   }, numeric(1))
   if (!is.null(sigma)) {
-    set.seed(seed)
     x2 <- x2 + rnorm(n, mean = 0, sd = sigma)
   }
   x2
@@ -21,9 +20,11 @@ new_df <- df
 new_df[["hml_encoded"]] <- encode_leave_one_out(df[["hml"]], df[["target"]])
 new_df[["city_encoded"]] <- encode_leave_one_out(df[["city"]], df[["target"]])
 head(new_df)
+set.seed(77)
 new_df[["city_encoded"]] <- encode_leave_one_out(df[["city"]], df[["target"]],
-                                                 sigma = 0.05, seed = 77)
+                                                 sigma = 0.05)
 head(new_df)
+set.seed(1123)
 new_df[["city_encoded"]] <- encode_leave_one_out(df[["city"]], df[["target"]],
-                                                 sigma = 0.05, seed = 1123)
+                                                 sigma = 0.05)
 head(new_df)
